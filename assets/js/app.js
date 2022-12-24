014d412992d14b36ae0808154b84717e39499cd2
skdev <skdev@ucsc.edu>
2022-12-24 13:22:23 -0800

    burger menu

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
